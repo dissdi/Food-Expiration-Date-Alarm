ceab0eb9ebe3b5a01f86eac7fe224a5739531690
dissdi <103642312+dissdi@users.noreply.github.com>
2022-11-27 15:50:44 +0900

    Add SQL database #1

--- app/src/main/java/com/example/teamproject03/model/Food.java
+++ app/src/main/java/com/example/teamproject03/model/Food.java
@@ -1,24 +1,23 @@
 package com.example.teamproject03.model;
 
-public class food {
+public class Food {
     private String name;
-    private int buyDate;
-    private int curDate;
-    private int dueDate;
+//    private int buyDate;
+//    private int curDate;
+//    private int dueDate;
     private int leftDate;
     private String storageType;
-    private int code;
+//    private int code;
     private String description;
     private int level;
     static final String[] COLOR = {"#DDDDDD", "#FFDBDB", "#FF6363", "#FF0000", "#111111"};
     private String color;
-    public food(String name, int leftDate, int buyDate, int dueDate, String storageType){
+    public Food(String name){
         this.name = name;
-        this.buyDate = buyDate;
-        this.curDate = buyDate; // change later
-        this.dueDate = dueDate;
-        this.leftDate = dueDate-curDate;
-        this.storageType = storageType;
+//        this.buyDate = buyDate;
+//        this.curDate = buyDate; // change later
+//        this.dueDate = dueDate;
+        setLeftDate();
         setDescription();
         setColor();
     }
@@ -26,33 +25,30 @@ public class food {
     public void setName(String name)    {
         this.name = name;
     }
-    public void setBuyDate(int buyDate) {
-        this.buyDate = buyDate;
-    }
+
     public void setLeftDate(int leftDate){
         this.leftDate = leftDate;
     }
-    public void setCurDate(int curDate){
-        this.curDate = curDate;
-    }
-    public void setDueDate(int dueDate){
-        this.dueDate =  dueDate;
-    }
+//    public void setCurDate(int curDate){
+//        this.curDate = curDate;
+//    }
+//    public void setDueDate(int dueDate){
+//        this.dueDate =  dueDate;
+//    }
     public void setStorageType(String storageType){
         this.storageType = storageType;
     }
-    public void setCode(int hash) { this.code = hash; }
+//    public void setCode(int hash) { this.code = hash; }
     public void setDescription(){
-        leftDate = dueDate - curDate;
         if(leftDate>=20) {
             this.description = "안전";
             this.level = 0;
         }
-        else if(leftDate>=10) {
+        else if(leftDate>=7) {
             this.description = "유의";
             this.level = 1;
         }
-        else if(leftDate>=5) {
+        else if(leftDate>=3) {
             this.description = "주의";
             this.level = 2;
         }
@@ -76,11 +72,11 @@ public class food {
 
     public String getName()         { return name; }
     public Integer getLeftDate()    { return leftDate; }
-    public Integer getCurDate()     { return curDate; }
-    public Integer getDueDate()     { return dueDate; }
-    public Integer getBuyDate()     { return buyDate; }
+//    public Integer getCurDate()     { return curDate; }
+//    public Integer getDueDate()     { return dueDate; }
+//    public Integer getBuyDate()     { return buyDate; }
     public String getStorageType()  { return storageType; }
-    public Integer getHashCode()    { return code; }
+//    public Integer getHashCode()    { return code; }
     public String getDescription()  { return description; }
     public String getColor()        {return color; }
     public int getLevel()           { return level; }
